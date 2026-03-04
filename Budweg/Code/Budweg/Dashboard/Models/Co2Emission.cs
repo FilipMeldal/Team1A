ecfcdf987af77c0e2fa15bfd6635ce62ce9fbc0b
spykiller103 <nbje74801@edu.ucl.dk>
2026-03-04 08:34:13 +0100

    Opdateret DCD, og startet på koden

---
+++ Budweg/Code/Budweg/Dashboard/Models/Co2Emission.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dashboard.Models
+{
+    public class Co2Emission
+    {
+    }
+}
---
+++ Budweg/Code/Budweg/Dashboard/ViewModels/MainViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dashboard.ViewModels
+{
+    internal class MainViewModel
+    {
+    }
+}
